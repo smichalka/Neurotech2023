4f695e49c4edeb799bbc96397f98a86ebf84d0b6
Sam <smichalka@users.noreply.github.com>
2023-10-11 20:34:48 -0400

    quick fix to markerlabels

--- Neurotech_Pison_Pipeline/epochFromMarkersToLabels.m
+++ Neurotech_Pison_Pipeline/epochFromMarkersToLabels.m
@@ -13,16 +13,20 @@ function [epochedData,gest_list] = epochFromMarkersToLabels(lsl_data,marker_data
 % Find all markers that indicate a rerecording and delete them
 bad_marker = 99; % This is the marker number to indicate the trial was re-recorded due to error
 % This is list of any markers that indicate a bad marker
-idx_bad_markers = find(marker_data(:,2)~=bad_marker);
-% Make list to include this index and the prior index (where the actual
-% trial starts)
-idx_bad_markers = [idx_bad_markers; (idx_bad_markers-1)];
-if min(idx_bad_markers) < 1
-    warning('Getting bad marker as first marker. Look at data for dropped markers')
-    idx_bad_markers(idx_bad_markers<=0) = 1; % 
+idx_bad_markers = find(marker_data(:,2)==bad_marker);
+
+% If there are bad markers
+if size(idx_bad_markers,1) > 0
+    % Make list to include this index and the prior index (where the actual
+    % trial starts)
+    idx_bad_markers = [idx_bad_markers; (idx_bad_markers-1)];
+    if min(idx_bad_markers) < 1
+        warning('Getting bad marker as first marker. Look at data for dropped markers')
+        idx_bad_markers(idx_bad_markers<=0) = 1; % 
+    end
+    % Remove all bad markers and their trials from the marker data
+    marker_data(idx_bad_markers,:) = [];
 end
-% Remove all bad markers and their trials from the marker data
-marker_data(idx_bad_markers) = [];
 
 
 % Find the Marker onset for all non-zero markers
