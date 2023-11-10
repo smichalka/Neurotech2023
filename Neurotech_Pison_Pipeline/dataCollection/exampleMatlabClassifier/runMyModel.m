ce6e6515ea127fb0905e1c7da04a3a4f06a30168
Sam <smichalka@users.noreply.github.com>
2023-11-10 14:03:09 -0500

    fix runMyModel

--- Neurotech_Pison_Pipeline/dataCollection/exampleMatlabClassifier/runMyModel.m
+++ Neurotech_Pison_Pipeline/dataCollection/exampleMatlabClassifier/runMyModel.m
@@ -3,22 +3,25 @@ function [res] = runMyModel(data)
 %   res is the result. 1 for rock, 2 for paper, 3 for scissors
 numCh = 4;
 
+% This is your classifier (currentClassifier, or whatever you named it)
+mymodel = load('simpleVarLDA.mat');
+
 filt_data = zeros(size(data,1),numCh);
 % Filter the data
 for ch = 1:numCh
     filt_data(:,ch) = highpass(data(:,1+ch), 5,1000);
 end
 
-feats = extractFeaturesExample(data',1:4,1000);
+feats = extractFeaturesExample(filt_data',1:4,1000);
 
-% This is your classifier (currentClassifier, or whatever you named it)
-mymodel = load('simpleVarLDA.mat');
+
 
 % You might want to confirm that mymodel.PredictorNames matches the feature
 % name in feats
 
 % You might want to check to see if the output of this is a 1,2, or 3. It
 % should be a number.
+
 res = mymodel.predict(feats);
 
 end
